64000dd0555c280e8095ee826ada22a1f2894ec1
Ernesto <theernestgames@gmail.com>
2025-03-16 20:15:14 +0100

    Añadido botones para cambiar de escena.

--- js/Game.js
+++ js/Game.js
@@ -2,12 +2,16 @@ class Game{
 
     #Scenes = [];
     #ActualScene = 0;
-    #Container;
-    #NextButton;
-    #PreviousButton;
+    #Container = null;
+    #NextButton = null;
+    #PreviousButton = null;
 
     constructor(Escena){
         this.#Container = document.querySelector(Escena);
+        this.#NextButton = this.#Container.querySelector(".NextButton");
+        this.#PreviousButton = this.#Container.querySelector(".PreviousButton");
+        this.#NextButton.addEventListener("click", this.#siguiente);
+        this.#PreviousButton.addEventListener("click", this.#anterior);
         for(const child of this.#Container.querySelectorAll(".scene")){
             var id = child.getAttribute("id")
             var scene = null;
@@ -24,35 +28,31 @@ class Game{
                 case "End":
                     scene = new End(child);
                     break;
-                if(scene != null){
-                    this.#Scenes.push(scene);
-                }
-                this.#update;
+            }
+            if(scene != null){
+                this.#Scenes.push(scene);
+                console.log(this.#Scenes)
             }
         }
-        this.#NextButton = this.#Container.querySelector(".NextButton");
-        this.#PreviousButton = this.#Container.querySelector(".PreviousButton");
-        this.#NextButton.addEventListener("click", this.#siguiente);
-        this.#PreviousButton.addEventListener("click", this.#anterior);
+        this.#update();
     }
 
     #update = () =>{
         this.#Scenes.forEach((element, index) =>{
-            element._container.classList.remove("active");
+            element._Container.classList.remove("active");
             if(index == this.#ActualScene){
-                element._container.classList.add("active");
+                element._Container.classList.add("active");
             }
-        }
-        )
+        });
     }
     #anterior = () =>{
+        console.log("adios")
         this.#ActualScene > 0 ? this.#ActualScene-- : 0;
-        this.#update
+        this.#update();
     }
-
     #siguiente = () =>{
-        this.#ActualScene < (this.#Scenes.length - 1) ? this.#ActualScene++ : 0;
-        this.#update;
+        console.log("hola")
+        this.#ActualScene < (this.#Scenes.length - 1) ? this.#ActualScene++ : this.#ActualScene=0;
+        this.#update();
     }
-
 }
