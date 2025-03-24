3c1a47ab8e98db7f93389a7008d32a6adcd724a9
Ernesto <theernestgames@gmail.com>
2025-03-24 12:54:50 +0100

    WIP: .

--- js/Config.js
+++ js/Config.js
@@ -2,44 +2,54 @@ class Config extends Scene{
     
     #LabelName1 = null;
     #LabelName2 = null;
+    #ImageList1 = null;
+    #ImageList2 = null;
+    #ImagenURL1 = null;
+    #ImagenURL2 = null;
     
     constructor(container){
         super(container);
 
         this.#LabelName1 = document.getElementById("LabelName1");
         this.#LabelName2 = document.getElementById("LabelName2");
-
+        this.#ImageList1 = document.getElementById("imageList1");
+        this.#ImageList2 = document.getElementById("imageList2");
+        this.#createList()
         
     }
 
     #PlayerSet(){
         var options = {
             player1: {
-                nombre: this.#LabelName1.value;
-                src:
-            },
-            player2:{
-                nombre:
-                src:
+                nombre: this.#LabelName1.value,
+                src: this.#ImagenURL1.src
             }
         }
     }
 
-    #createList() {
-        fetch("https://dragonball-api.com/api/characters?page=1&limit=10")
+    async #createList() {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
             .then((response) => response.json())
-            .then((data) => {
-                for (var i = 0; i < data.meta.itemCount; i++) {
-                    var img1 = document.createElement("img");
-                    img1.src = data.items[i].image;
-                    img1.addEventListener("click", this.#selectImg1)
-                    imageList1.appendChild(img1);
-                    var img2 = document.createElement("img");
-                    img2.src = data.items[i].image;
-                    imageList2.appendChild(img2);
-                    img2.addEventListener("click", this.#selectImg2)
+            .then(async (data) => {
+                for (var i = 0; i < data.results.length; i++) {
+                    await fetch(data.results[i].url)
+                        .then((response) => response.json())
+                        .then((pokemon) => {
+                            var img1 = document.createElement("img");
+                            img1.src = pokemon.sprites.front_default;
+                            img1.addEventListener("click", this.#selectImg1)
+                            this.#ImageList1.appendChild(img1);
+                        });
                 }
             });
+    }
+    #selectImg1 = () => {
+        if (this.#ImagenURL1 != null) {
+
+            this.#ImagenURL1.style.backgroundColor = ""
+        }
+        this.#ImagenURL1 = event.target;
+        this.#ImagenURL1.style.backgroundColor = "cyan";
     }
     start(){
 
